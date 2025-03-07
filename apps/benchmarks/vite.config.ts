171153858539ed7206298038b5a746dc512aa494
kyngwan@gmail.com <kyngwan@gmail.com>
2025-03-07 18:36:45 +0900

    fix(benchmarks): change configs of vite, tsconfig

--- apps/benchmarks/vite.config.ts
+++ apps/benchmarks/vite.config.ts
@@ -1,12 +1,12 @@
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     optimizeDeps: {
         esbuildOptions: {
+            target: "esnext",
             define: {
                 global: "globalThis"
             },
