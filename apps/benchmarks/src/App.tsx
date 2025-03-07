5b4971ca942009ed2a679f88d968c1b1fe0a9375
kyngwan@gmail.com <kyngwan@gmail.com>
2025-03-07 18:40:04 +0900

    feat(benchmarks): add todos

--- apps/benchmarks/src/App.tsx
+++ apps/benchmarks/src/App.tsx
@@ -11,6 +11,8 @@ import initNoirC from "@noir-lang/noirc_abi"
 import initACVM from "@noir-lang/acvm_js"
 import acvm from "@noir-lang/acvm_js/web/acvm_js_bg.wasm?url"
 import noirc from "@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm?url"
+// TODO: use generateProof of 'semaphore-proof-noir-test-proof',
+// TODO: for now, the wasm is not loaded within this module when generateProof
 import generateProofV4Noir from "./semaphore-noir/generate-proof"
 import TreeDepth from "./components/TreeDepth"
 import Navbar from "./components/Navbar"
