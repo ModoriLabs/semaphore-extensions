74a0a2f6aaecb2c36efd4236e5964324a41e15c6
kyngwan@gmail.com <kyngwan@gmail.com>
2025-03-07 18:58:08 +0900

    fix(benchmarks): fix build error
    update typescript version to 5 for using moduleResolution: 'Bundler'

--- apps/benchmarks/vite.config.ts
+++ apps/benchmarks/vite.config.ts
@@ -1,9 +1,10 @@
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import topLevelAwait from "vite-plugin-top-level-await"
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), topLevelAwait()],
     optimizeDeps: {
         esbuildOptions: {
             target: "esnext",
